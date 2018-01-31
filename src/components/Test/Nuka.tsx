a441fa0f2dfc84f591b9a2a2c277ef1f7f5d9640
Jonah Nicholaus Pereira <jonah.pereira@agoda.com>
2018-01-31 14:33:11 +0700

    Save changes nuka

---
+++ src/components/Test/Nuka.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Carousel from 'nuka-carousel';
+
+interface Props { }
+interface State { }
+
+export class Nuka extends React.Component<Props, State> {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <Carousel>
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
+                <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+            </Carousel>
+        );
+    }
+}
--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,138 +1,18 @@
-// import * as React from 'react';
-// import ReactCSSTransitionGroup = require('react-addons-css-transition-group');
-// import { Child } from './Child';
-// import ReactCSSTransitionReplace = require('react-css-transition-replace');
-
-// import "./Test.scss";
-
-// export interface Props {
-//     texts: string[],
-// }
-
-// interface State {
-//     hover: boolean,
-//     text: string,
-// }
-
-// export class Test extends React.Component<Props, State> {
-//     constructor(props) {
-//         super(props);
-
-//         this.state = {
-//             hover: false,
-//             text: this.props.texts[0],
-//         };
-
-//         this.setInterval();
-//     }
-
-//     private interval: number;
-//     private i = 0;
-
-//     private setInterval = () => {
-//         this.interval = setInterval(() => {
-//             this.setState({ text: this.props.texts[this.i] });
-//             this.i += 1;
-//             if (this.i == this.props.texts.length) this.i = 0;
-//         }, 2000);
-//     };
-
-//     onMouseOver = () => {
-//         clearInterval(this.interval);
-//         this.i = 0;
-//         this.setState({ hover: true });
-//     };
-
-//     onMouseOut = () => {
-//         this.setState({ hover: false });
-//         this.setInterval();
-//     };
-
-//     onGetContent = () => {
-//         if (this.state.hover) return <div>Hovered</div>;
-
-//         return (
-//             <div className="childMainDiv">
-//                  {/* <ReactCSSTransitionGroup transitionName="fade-wait"
-//                     transitionEnterTimeout={500}
-//                     transitionAppearTimeout={500}
-//                     transitionLeaveTimeout={300}>
-//                     <Child key={this.state.text} text={this.state.text} />
-//                 </ReactCSSTransitionGroup>  */}
-//                 <ReactCSSTransitionReplace transitionName="fade-wait"
-//                     transitionEnterTimeout={500} transitionLeaveTimeout={500}>
-//                     <Child key={this.state.text} text={this.state.text} />
-//                 </ReactCSSTransitionReplace>
-//             </div>
-//         );
-//     };
-
-//     render() {
-//         return (
-//             <div className="mainDiv">
-//                 {this.onGetContent()}
-//             </div>
-//         )
-//     };
-// };
-
 import * as React from 'react';
-import { Fader } from './Fader';
+import { Nuka } from './Nuka'
 
-import "./Test.scss";
+interface Props { }
+interface State { }
 
-export class Test extends React.Component<any, any> {
+export class Test extends React.Component<Props, State> {
     constructor(props) {
         super(props);
-        this.state = {
-            components: [
-                <div className="child1" key="one"><div>1</div>&nbsp;<div>2</div></div>,
-                <div className="child1" key="two">Two sadfsad</div>,
-                <div className="child1" key="three">Three d</div>
-            ],
-            length: 0,
-            hover: false,
-        };
+
     }
-
-    toggleComponent = () => {
-        this.setState({
-            length: (this.state.length == 2) ? 0 : (this.state.length + 1),
-        });
-
-    };
-
-    componentDidMount() {
-        setInterval(this.toggleComponent, 2000);
-    }
-
-    getContent = () => {
-        if (this.state.hover) return <div>Hovered</div>;
-
-        return (
-            <Fader>
-                {this.state.components[this.state.length]}
-            </Fader>
-        );
-    };
-
-    onMouseOver = () => {
-        this.setState({ hover: true });
-    };
-
-    onMouseOut = () => {
-        this.setState({ hover: false });
-    };
 
     render() {
-
         return (
-            <div className="App" onMouseEnter={this.onMouseOver} onMouseLeave={this.onMouseOut} >
-                <div className="child">
-                    {this.getContent()}
-                </div>
-            </div>
+            <Nuka />
         );
     }
 }
-
